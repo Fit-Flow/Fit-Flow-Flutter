2c01f45a519e993029d764093684eeb3f255942a
Christoffer <christofferp97@hotmail.com>
2023-05-10 12:30:35 +0200

    FF-12: Tilføjet seneste træning's boxe

--- lib/view/dashboard/start_page.dart
+++ lib/view/dashboard/start_page.dart
@@ -2,6 +2,7 @@ import 'package:fit_flow_flutter/utils/app_colors.dart';
 import 'package:fit_flow_flutter/utils/components/graph_activity.dart';
 import 'package:flutter/material.dart';
 import '../../utils/components/graph_overview.dart';
+import '../../utils/components/header_widget.dart';
 
 class StartPage extends StatelessWidget {
   StartPage({Key? key}) : super(key: key);
@@ -13,14 +14,8 @@ class StartPage extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            child: Text(
-              'Oversigt',
-              style: TextStyle(
-                  color: AppColors.textColor,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w900),
-            ),
+          HeaderWidget(
+            title: "Oversigt",
           ),
           SizedBox(
             height: 15,
@@ -46,18 +41,49 @@ class StartPage extends StatelessWidget {
           SizedBox(
             height: 25,
           ),
-          Container(
-            child: Text(
-              'Aktivitet',
-              style: TextStyle(
-                  color: AppColors.textColor,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w900),
-            ),
+          HeaderWidget(
+            title: "Aktivitet",
           ),
-          SizedBox(height: 15,),
+          SizedBox(
+            height: 15,
+          ),
           GraphActivity(
             yTitle: "Dage",
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          HeaderWidget(
+            title: "Seneste træninger",
+          ),
+          SizedBox(height: 15,),
+          Container(
+            height: 150, width: 320,
+            padding: EdgeInsets.all(20), // Set padding around the container
+            decoration: BoxDecoration(
+              color: AppColors.darkGreyColor, // Set background color
+              borderRadius: BorderRadius.circular(8.0), // Set border radius
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Træning 24-06/23",
+                  style: TextStyle(color: AppColors.grayTextColor, fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10,),
+                Wrap(
+                  spacing: 10,
+                  runSpacing: 10,
+                  children: List.generate(5, (index) => 
+                    Text(
+                  "Øvelser her",
+                  style: TextStyle(color: AppColors.grayTextColor, fontSize: 16,),
+                ),
+                  )
+                ),
+              ],
+            ),
           )
         ],
       ),
