a45154c4a9d4681419e207c448f5924cd35ad332
jakobmajiddd <jakobmajid00@gmail.com>
2023-05-11 13:27:46 +0200

    FF-21: Tilføjet trainingfield component

--- lib/view/dashboard/training_page.dart
+++ lib/view/dashboard/training_page.dart
@@ -1,9 +1,11 @@
 import 'package:fit_flow_flutter/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 import '../../utils/components/header_widget.dart';
 import '../../utils/components/search_field.dart';
+import '../../utils/components/training_field.dart';
 import '../../utils/components/workout_field.dart';
 import '../../utils/components/workout_searchlist.dart';
 
@@ -25,12 +27,34 @@ class TrainingPage extends StatelessWidget {
             SizedBox(
               height: 15,
             ),
-            WorkoutField(
-              workout: 'Bænkpres',
-              onTap: () {
-                buildWorkoutDialog();
-              },
-            ),
+            Row(
+              children: [
+                WorkoutField(
+                  workout: 'Bænkpres',
+                  onTap: () {
+                    buildWorkoutDialog();
+                  },
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        TrainingField(
+                          hintText: 'Indtast din vægt',
+                          prefixText: 'Vægt',
+                          sufixText: 'Kg',
+                        ),
+                        TrainingField(
+                          hintText: 'Indtast din reps',
+                          prefixText: 'Reps',
+                          sufixText: '',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            )
           ],
         ),
       ),
